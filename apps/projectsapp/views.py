a2ce8aa2e22bb1b4eb9fc94174c7bc9a76e9b04c
Egor-oop <gulidoegor@gmail.com>
2023-03-04 14:53:08 +0300

    feat: Projects at 'api/projects'

--- apps/projectsapp/views.py
+++ apps/projectsapp/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated
 
-# Create your views here.
+from .serializers import ProjectSerializer
+from .models import Project
+
+
+class ProjectViewSet(viewsets.ModelViewSet):
+    queryset = Project.objects.all()
+    serializer_class = ProjectSerializer
+    permission_classes = (IsAuthenticated, )
+
