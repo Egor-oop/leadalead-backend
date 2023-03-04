634fff79b88858348a17a0b3322dfe795a207b01
Egor-oop <gulidoegor@gmail.com>
2023-03-04 15:16:53 +0300

    fix: added auto adding user and IsOwnerOrReadOnly permission to ProjectViewSet

--- apps/projectsapp/views.py
+++ apps/projectsapp/views.py
@@ -1,6 +1,8 @@
 from rest_framework import viewsets
 from rest_framework.permissions import IsAuthenticated
 
+from apps.users.permissions import IsOwnerOrReadOnly
+
 from .serializers import ProjectSerializer
 from .models import Project
 
@@ -8,5 +10,7 @@ from .models import Project
 class ProjectViewSet(viewsets.ModelViewSet):
     queryset = Project.objects.all()
     serializer_class = ProjectSerializer
-    permission_classes = (IsAuthenticated, )
+    permission_classes = (IsAuthenticated, IsOwnerOrReadOnly)
 
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
