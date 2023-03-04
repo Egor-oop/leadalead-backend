8b390541015b722fa032371997ddacdc3a554e1d
Egor-oop <gulidoegor@gmail.com>
2023-03-04 14:52:40 +0300

    feat: Project model

--- apps/projectsapp/models.py
+++ apps/projectsapp/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Project(models.Model):
+    name = models.CharField(max_length=100, blank=False, null=False)
+    user = models.ForeignKey(User, on_delete=models.SET_NULL, blank=False, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
