08f177705e54aad5eead0d90c6c079e78db92447
Egor-oop <gulidoegor@gmail.com>
2023-03-06 22:02:30 +0300

    get lead by project id

--- apps/leadsapp/views.py
+++ apps/leadsapp/views.py
@@ -6,3 +6,9 @@ from .serializers import LeadSerializer
 class LeadViewSet(viewsets.ModelViewSet):
     queryset = Lead.objects.all()
     serializer_class = LeadSerializer
+
+    def get_queryset(self):
+        project_id = self.request.query_params.get('projectid', None)
+        if project_id is not None:
+            return Lead.objects.filter(project_id=project_id)
+        return self.queryset
